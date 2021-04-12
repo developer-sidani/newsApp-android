cbd949f84b8501a453eaf92ae7fc403bbe8604af
aliyassine26 <aliyassine_2000@hotmail.com>
2021-04-12 22:03:05 +0300

    icons

--- app/src/main/java/com/example/ahmadsidani20190148aliyassine20190234hadiibrahim20170297/ListAdapter.java
+++ app/src/main/java/com/example/ahmadsidani20190148aliyassine20190234hadiibrahim20170297/ListAdapter.java
@@ -39,23 +39,31 @@ public class ListAdapter extends ArrayAdapter  {
 
 
         if(n.getCategory().equals("Breaking News")){
-//            newsImage.setImageResource(R.mipmap.breaking_news_round);
+            newsImage.setImageResource(R.mipmap.breakingnews_round);
         }
         else if(n.getCategory().equals("Health")){
-            newsImage.setImageResource(R.drawable.health);
+            newsImage.setImageResource(R.mipmap.healthnews_round);
         }
         else if(n.getCategory().equals("Sports")){
-            newsImage.setImageResource(R.drawable.sports);
+            newsImage.setImageResource(R.mipmap.sportsnews_round);
         }
         else if(n.getCategory().equals("Technology")){
-            newsImage.setImageResource(R.drawable.technology);
+            newsImage.setImageResource(R.mipmap.technologynews_round);
         }
-        else if(n.getCategory().equals("Travel")){
+        else if(n.getCategory().equals("Economics")){
+
+            newsImage.setImageResource(R.mipmap.economicsnews_round);
+        }
+        else if(n.getCategory().equals("Local")){
+            newsImage.setImageResource(R.mipmap.localnews_round);
             //okay
-            newsImage.setImageResource(R.drawable.travel);
         }
-        else if(n.getCategory().equals("Travel")){
-            newsImage.setImageResource(R.drawable.travel);
+        else if(n.getCategory().equals("Global")){
+            newsImage.setImageResource(R.mipmap.globalnews_round);
+            //okay
+        }
+        else if(n.getCategory().equals("Politics")){
+            newsImage.setImageResource(R.mipmap.politicsnews_round);
             //okay
         }
 
